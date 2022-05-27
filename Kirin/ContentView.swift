b595735b350d15007ae5bcfd02e86cee46cabf18
Ali Jaffar <alij123402@gmail.com>
2022-05-26 21:18:44 -0400

    Initial Commit

---
+++ Kirin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kirin
+//
+//  Created by Ali Jaffar on 2022-05-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kirin/KirinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KirinApp.swift
+//  Kirin
+//
+//  Created by Ali Jaffar on 2022-05-26.
+//
+
+import SwiftUI
+
+@main
+struct KirinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
