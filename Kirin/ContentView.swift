a0dace65ad20f07f8e67cabcf74648206bcf893c
Ali Jaffar <alij123402@gmail.com>
2022-06-17 20:58:50 -0400

    Added a launch screen as well as a text block(experimenting the colours and how it works). Will be update heavily for the next few days

--- Kirin/ContentView.swift
+++ Kirin/ContentView.swift
@@ -6,16 +6,87 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    
+    @State private var fadeInOut = false
+    @State private var showImage: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+         ZStack {
+            NavigationView {
+                    Text("Kirin")
+                        .onAppear() {
+                            withAnimation(Animation.easeInOut(duration: 0.6)) {
+                                    fadeInOut.toggle()
+                                }
+                            
+                        }.opacity(fadeInOut ? 0 : 1)
+                        .padding()
+                        .font(.largeTitle.weight(.bold))
+                        .frame(width: 300, height: 300, alignment: .topLeading)
+                        .foregroundColor(Color("Color1"))
+                        .background(
+                            RoundedRectangle(cornerRadius: 20,style: .circular) .fill(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [.gray, .red]),
+                                    startPoint: .leading,
+                                    endPoint: .trailing
+                            
+                            )
+                        )
+                    )
+                NavigationView{
+                    Text("Kirin")
+                        .font(.largeTitle.weight(.bold))
+                        .frame(width: 300, height: 300, alignment: .topLeading)
+                    
+                }
+
+ }
+             
+             NavigationView {
+                 Text("Kirin")
+                     .onAppear() {
+                         withAnimation(Animation.easeIn(duration:3)) {
+                             fadeInOut.toggle()
+                         }
+                     }
+                     .font(.largeTitle.weight(.bold))
+                     .frame(width: 350, height: 500)
+                     .foregroundColor(Color("Color1"))
+                     .background(
+                         RoundedRectangle(cornerRadius: 20,style: .circular) .fill(
+                             LinearGradient(
+                                 gradient: Gradient(colors: [.gray, .red]),
+                                 startPoint: .leading,
+                                 endPoint: .trailing
+                         
+                         )
+                     )
+                 )
+             }
+             
+             
     }
+        
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            .previewInterfaceOrientation(.portrait)
+            
+        
     }
 }
+
+}
+
+
